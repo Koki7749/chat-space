ab3d3b90be0b7e2551bb805f5b26f8055d6816b2
Koki7749 <ito8864@gmail.com>
2019-08-28 09:22:24 +0900

    database

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,21 @@
 class GroupsController < ApplicationController
+
+def new
+end
+
+def create
+  Groups.create(name: group_params[:name])
+end
+
+def edit
+  @group = Groups.find(params[:id])
+
+end
+
+def update
+end
+
+private
+def group_params
+  params.permit(:name)
 end
