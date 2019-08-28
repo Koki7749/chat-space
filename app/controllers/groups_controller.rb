3ad64d485bc01f010b8a5a7655c66b5f65f35aaf
Koki7749 <ito8864@gmail.com>
2019-08-28 16:21:29 +0900

    finish

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,15 +18,24 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Groups.find(params[:id])
+    @group = Group.find(params[:id])
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'posts#index'
+  root 'posts#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
+    resources :posts, only: [:index, :create]
 end
 
 end
