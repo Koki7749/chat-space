fa8cddfb39faf20d83de4ce9640956da3da62066
Koki7749 <ito8864@gmail.com>
2019-08-29 16:32:13 +0900

    group

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,15 @@ class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
   has_many :posts
+
+  validates :name, presence: true
+
+  def show_last_post
+    if (last_post = posts.last).present?
+      last_post.body? ? last_post.body : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true, unless: :image?
+  mount_uploader :image, ImageUploader
 end
