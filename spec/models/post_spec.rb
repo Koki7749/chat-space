1bbfb7919a1ed469d74f2d16bf7ca0fd0c556e45
Koki7749 <ito8864@gmail.com>
2019-08-30 17:43:11 +0900

    test

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe '#create' do
+    context 'can save' do
+      it 'is valid with body' do
+        expect(build(:post, image: nil)).to be_valid
+      end
+
+      it 'is valid with image' do
+        expect(build(:post, body: nil)).to be_valid
+      end
+
+      it 'is valid with body and image' do
+        expect(build(:post)).to be_valid
+      end
+    end
+
+    context 'can not save' do
+      it 'is invalid without body and image' do
+        post = build(:post, body: nil, image: nil)
+        post.valid?
+        expect(post.errors[:body]).to include('を入力してください')
+      end
+
+      it 'is invalid without group_id' do
+        post = build(:post, group_id: nil)
+        post.valid?
+        expect(post.errors[:group]).to include('を入力してください')
+      end
+
+      it 'is invaid without user_id' do
+        post = build(:post, user_id: nil)
+        post.valid?
+        expect(post.errors[:user]).to include('を入力してください')
+      end
+    end
+  end
+end
