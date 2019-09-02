3ca685b086b01ad650bd8d6f4cf4d7f335257eaf
Koki7749 <ito8864@gmail.com>
2019-09-02 19:23:08 +0900

    edit done method

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,14 +1,37 @@
 $(function() {
-  $("#user-search-field").on("keyup", function() {
-    var input = $("#user-search-field").val();
+  function appendUser(user){
+    var html=`
+    <div class="chat-group-user clearfix">
+    <p class="chat-group-user__name">${user.name}</p>
+    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+    </div>`
+    return html;
+  }
 
+  $("#user-search-field").on("keyup", function(e) {
+    // e.preventDefault();
+    var input = $("#user-search-field").val();
+    
     $.ajax({
       type: 'GET',
       url: '/users',
-      data: { keyword: input },
-      dataType: 'json'
+      data: { name: input },
+      dataType: 'json',
     })
-    console.log(this)
 
+    .done(function(user){
+      console.log(user);
+      if (user.length !== 0) {
+        user.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendErrMsgToHTML("一致するユーザーが見つかりません");
+      }
+    })
+    .fail(function(){
+      alert('error');
+    })
   });
 });
--- app/views/users/user.json.jbuilder
+++
@@ -1,3 +0,0 @@
-json.user do |json|
-  json.name @user.name
-end
