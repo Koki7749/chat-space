d182e5b9f37fc2aab54785737e7bc62a5f8e7b05
Koki7749 <ito8864@gmail.com>
2019-09-05 21:27:16 +0900

    edit post-id

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -2,7 +2,7 @@ $(document).on('turbolinks:load', function(){
   function buildHTML(post){
 
     var image = post.image? post.image : ""
-    var html = `<div class="chat">
+    var html = `<div class="chat_message" data-id=${post.id}">
                   <div class="chat-member">
                     ${post.user_name}
                   </div>
@@ -51,3 +51,62 @@ $(document).on('turbolinks:load', function(){
     })
   })
 })
+
+
+
+$(function() {
+  
+  function buildHTML(post){
+
+    var image = post.image? post.image : ""
+    var html = `<div class="chat_message" data-id=${post.id}">
+                  <div class="chat-member">
+                    ${post.user_name}
+                  </div>
+                  <div class="chat-time">
+                    ${post.created_at}
+                  </div>
+                  <div class="chat-comment">
+                  <p class="lower-post__body">
+                    ${post.body}
+                  </p>
+                  </div>
+                  <div>
+                  <img class="lower-message__image"
+                    src=${image}>
+                  </div>
+                </div>`
+    return html;
+  };
+
+    var reloadPosts = function() {
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      last_post_id = $(".chat-home .chat id:last-child").data("data");
+
+      $.ajax({
+        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+        url: '/api/posts',
+        //ルーティングで設定した通りhttpメソッドをgetに指定
+        type: 'get',
+        dataType: 'json',
+        //dataオプションでリクエストに値を含める
+        data: {id: last_post_id}
+      })
+      .done(function(posts) {
+        console.log(posts);
+        //console.log("test")
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        posts.forEach(function(post){
+          insertHTML = buildHTML(post);
+          $('.chat-home').append(insertHTML);
+          // $('.chat-home').animate({ scrollTop: $('.chat-home')[0].scrollHeight});
+        });
+      })  
+      .fail(function() {
+        // alert('自動更新に失敗しました');
+    });
+  };
+  setInterval(reloadPosts, 5000);
+});
--- app/views/posts/create.json.jbuilder
+++ app/views/posts/create.json.jbuilder
@@ -3,3 +3,4 @@ json.image  @post.image.url
 json.user_id  @post.user_id
 json.created_at  @post.created_at.strftime("%Y年 %m月 %d日 %H時%M分")
 json.user_name  @post.user.name
+json.id @post.id
